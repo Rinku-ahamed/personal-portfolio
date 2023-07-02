a19722be37e719af381a7ae8ca07bf5244b07fa8
rinkuahamed <ringkuali18@gmail.com>
2023-07-02 13:07:40 +0600

    create preloader for this website

---
+++ src/shared/PreLoader/PreLoader.jsx
@@ -0,0 +1,24 @@
+import "./PreLoader.css";
+const PreLoader = () => {
+  return (
+    <div className="wrapper">
+      <div className="pl">
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__dot"></div>
+        <div className="pl__text">Loading…</div>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
